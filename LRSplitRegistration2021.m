452fa834da691999bd47cd042dc8dad2640ae7a2
Yeison Rodriguez <67986437+yeisonisradd@users.noreply.github.com>
2021-05-04 06:12:53 -0500

    Update LRSplitRegistration2021.m

--- LRSplitRegistration2021.m
+++ LRSplitRegistration2021.m
@@ -7,8 +7,10 @@ clear;
 %read in the file provided
 I = dicomread("20201208_2DpCASL.dcm");
 I = 100*squeeze(I);
+%save the size and number of images in the set
 [row, col, numImages] = size(I);
 
+%initialize sum variables which can be used to later find the average
 sumImagesLeftControl = 0;
 sumImagesRightControl = 0;
 sumImagesLeftLabel = 0;
@@ -19,16 +21,19 @@ for k = 1:numImages
     if k <= numImages/2
         %seperate into the two groups, first 16 are control
         dicomimgControl{k} = I(:,:,k);
+        %seperate into left and right side of the dynamic.
         leftControl{k} = dicomimgControl{k}(:,1:col/2);
         rightControl{k} = dicomimgControl{k}(:,col/2 +1:col);
+        %sum the images
         sumImagesLeftControl = sumImagesLeftControl + double(leftControl{k});
         sumImagesRightControl = sumImagesRightControl + double(rightControl{k});
     else
+        %repeat process for the label images
         dicomimgLabel{k-numImages/2} = I(:,:,k);
         leftLabel{k-numImages/2} = dicomimgLabel{k-numImages/2}(:,1:col/2);
         rightLabel{k-numImages/2} = dicomimgLabel{k-numImages/2}(:,col/2 + 1:col);
-        sumImagesLeftLabel = sumImagesLeftLabel + double(leftLabel{k-16});
-        sumImagesRightLabel = sumImagesRightLabel + double(rightLabel{k-16});
+        sumImagesLeftLabel = sumImagesLeftLabel + double(leftLabel{k-numImages/2});
+        sumImagesRightLabel = sumImagesRightLabel + double(rightLabel{k-numImages/2});
         
         
     end
@@ -41,7 +46,6 @@ end
 I = 0;
 
 %Calculate Average Control & Label
-
 AverageLeftControl = sumImagesLeftControl/16;
 AverageRightControl = sumImagesRightControl/16;
 AverageLeftLabel = sumImagesLeftLabel/16; 
@@ -51,50 +55,46 @@ AverageControl(:,1:col/2) = AverageLeftControl;
 AverageControl(:,col/2 + 1:col) = AverageRightControl;
 AverageLabel(:,1:col/2) = AverageLeftLabel;
 AverageLabel(:,col/2 + 1:col) = AverageRightLabel;
+%use the closest to average function to find the control and label index closest to their respective average.
 [controlIndex,labelIndex] = ClosestToAverage(AverageControl,AverageLabel,dicomimgControl,dicomimgLabel);
-[controlLeftIndex,labelLeftIndex] = ClosestToAverage(AverageLeftControl,AverageLeftLabel,leftControl,leftLabel);
-[controlRightIndex,labelRightIndex] = ClosestToAverage(AverageRightControl,AverageRightLabel,rightControl,rightLabel);
 
-
-AverageDiff = 0;
 for i = 1:numImages/2
-    
-    
- %create Registered Control & Label Images. Three transformations: Rigid, affine, & Non-Rigid
- [~,regLeftControl{i}] = elastix(leftControl{i},leftControl{controlIndex},[],{'RigidParameters.txt','AffineParameters.txt','NonRigidParameters.txt'});
- [~,regRightControl{i}] = elastix(rightControl{i},rightControl{controlIndex},[],{'RigidParameters.txt','AffineParameters.txt','NonRigidParameters.txt'});
- [~,regLeftLabel{i}] = elastix(leftLabel{i},leftLabel{labelIndex},[],{'RigidParameters.txt','AffineParameters.txt','NonRigidParameters.txt'});
- [~,regRightLabel{i}] = elastix(rightLabel{i},rightLabel{labelIndex},[],{'RigidParameters.txt','AffineParameters.txt','NonRigidParameters.txt'});
+    %create Registered Control & Label Images. Three transformations: Rigid, affine, & Non-Rigid
+    [~,regLeftControl{i}] = elastix(leftControl{i},leftControl{controlIndex},[],{'RigidParameters.txt','NonRigidParameters.txt'});
+    [~,regRightControl{i}] = elastix(rightControl{i},rightControl{controlIndex},[],{'RigidParameters.txt','NonRigidParameters.txt'});
+    [~,regLeftLabel{i}] = elastix(leftLabel{i},leftLabel{labelIndex},[],{'RigidParameters.txt','NonRigidParameters.txt'});
+    [~,regRightLabel{i}] = elastix(rightLabel{i},rightLabel{labelIndex},[],{'RigidParameters.txt','NonRigidParameters.txt'});
 
 end
-save TestingVarLR2015.mat
 
+%initialize sum variables to find the average registered image
 sumRegLeftControl = 0;
 sumRegRightControl = 0;
 sumRegLeftLabel = 0;
 sumRegRightLabel = 0;
 %sum each image to find the average
-for i = 1:16
-    sumRegLeftControl = sumRegLeftControl + double(regLeftControl{i}.transformedImages{3});
-    sumRegRightControl = sumRegRightControl + double(regRightControl{i}.transformedImages{3});
-    sumRegLeftLabel = sumRegLeftLabel + double(regLeftLabel{i}.transformedImages{3});
-    sumRegRightLabel = sumRegRightLabel + double(regRightLabel{i}.transformedImages{3});
+for i = 1:numImages/2
+    %sum each registered image
+    sumRegLeftControl = sumRegLeftControl + double(regLeftControl{i}.transformedImages{2});
+    sumRegRightControl = sumRegRightControl + double(regRightControl{i}.transformedImages{2});
+    sumRegLeftLabel = sumRegLeftLabel + double(regLeftLabel{i}.transformedImages{2});
+    sumRegRightLabel = sumRegRightLabel + double(regRightLabel{i}.transformedImages{2});
     
 end
 
 %calculate the average of the registered images
-AverageRegLeftControl = sumRegLeftControl/16;
-AverageRegRightControl = sumRegRightControl/16;
-AverageRegLeftLabel = sumRegLeftLabel/16;
-AverageRegRightLabel = sumRegRightLabel/16;
+AverageRegLeftControl = sumRegLeftControl/(numImages/2);
+AverageRegRightControl = sumRegRightControl/(numImages/2);
+AverageRegLeftLabel = sumRegLeftLabel/(numImages/2);
+AverageRegRightLabel = sumRegRightLabel/(numImages/2);
 
-[~,CoregLabelLeft] = elastix(AverageRegLeftLabel, AverageRegLeftControl, [], {'RigidParameters.txt','AffineParameters.txt'});
-[~,CoregLabelRight] = elastix(AverageRegRightLabel, AverageRegRightControl, [], {'RigidParameters.txt','AffineParameters.txt'});
+[~,CoregLabelLeft] = elastix(AverageRegLeftLabel, AverageRegLeftControl, [], {'RigidParameters.txt'});
+[~,CoregLabelRight] = elastix(AverageRegRightLabel, AverageRegRightControl, [], {'RigidParameters.txt'});
 
 AverageRegControl(:,1:col/2) = AverageRegLeftControl;
 AverageRegControl(:,col/2 + 1:col) = AverageRegRightControl;
-CoregLabel(:,1:col/2) = CoregLabelLeft.transformedImages{2};
-CoregLabel(:,col/2 + 1:col) = CoregLabelRight.transformedImages{2};
+CoregLabel(:,1:col/2) = CoregLabelLeft.transformedImages{1};
+CoregLabel(:,col/2 + 1:col) = CoregLabelRight.transformedImages{1};
 
 ASLImage2 = (abs(AverageRegControl - double(CoregLabel)));
 
